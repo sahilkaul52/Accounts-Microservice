7ea7dcf8b1eeca3a22fd46a1e1df673753830656
sahilkaul52 <sahilkaul52@gmail.com>
2024-03-16 13:44:09 +0530

    Reading dynamic config using @Value

--- src/main/java/com/example/accounts/controller/AccountsController.java
+++ src/main/java/com/example/accounts/controller/AccountsController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -30,11 +31,18 @@ import org.springframework.web.bind.annotation.*;
     )
     @RestController
     @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-    @AllArgsConstructor
+
     @Validated
     public class AccountsController {
 
         private IAccountsService iAccountsService;
+
+        public AccountsController(IAccountsService iAccountsService) {
+            this.iAccountsService = iAccountsService;
+        }
+
+        @Value("${build.version}")
+        private String buildVersion;
 
         @Operation(
                 summary = "Create Account REST API",
@@ -160,6 +168,31 @@ import org.springframework.web.bind.annotation.*;
                         .status(HttpStatus.EXPECTATION_FAILED)
                         .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
             }
+        }
+
+        @Operation(
+                summary = "Get Build information",
+                description = "Get Build information that is deployed into accounts microservice"
+        )
+        @ApiResponses({
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "HTTP Status OK"
+                ),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "HTTP Status Internal Server Error",
+                        content = @Content(
+                                schema = @Schema(implementation = ErrorResponseDto.class)
+                        )
+                )
+        }
+        )
+        @GetMapping("/build-info")
+        public ResponseEntity<String> getBuildInfo() {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(buildVersion);
         }
 
 
